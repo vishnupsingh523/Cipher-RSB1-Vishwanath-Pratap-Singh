6084cb272fcc9857e2aeb9030eda625cb0092545
Vishwanath Pratap Singh <vishnupsingh523@gmail.com>
2022-06-26 18:01:32 +0530

    files added

---
+++ src/components/Card.js
@@ -0,0 +1,11 @@
+function Card(props){
+    return (
+        <div>
+            <div className='card'>
+                <img src="images/image2.jpg"/>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
